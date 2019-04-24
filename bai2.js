817f271173109df0b436f614f61cb79c8d4b8c60
alone0x <49860410+alone0x@users.noreply.github.com>
2019-04-23 21:09:16 -0700

    1

---
+++ bai2.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+ inputwidth=prompt("nhap width ");
+ inputheight=prompt("nhap height");
+ let width = parseInt(inputwidth);
+ let height= parseInt(inputheight);
+ let area = width*height;
+ document.write("area="+area);
